b33317f1c5d5e117d903058426f031a111e1e93c
anwardarkazanli <anwar905@revature.net>
2022-12-03 08:57:11 -0800

    updated the cart page object model with xpaths for the cart feature

--- src/test/java/com/revature/pages/CartPage.java
+++ src/test/java/com/revature/pages/CartPage.java
@@ -10,36 +10,143 @@ public class CartPage {
     public CartPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
+
+    //Page urls
+    String carturlpage = "http://localhost:3000/cart";
+    String checkouturlpage = "http://localhost:3000/checkout";
+
+    //Address information
+    String fname = "Anwar";
+    String lname = "Darkazanli";
+    String address = "777 Orange St.";
+    String State = "New York";
+    String city = "Queens";
+    String zip = " 30024";
+    String country = "US";
+
+    //CreditCardInformation
+    String creditcardnumber = "abc";
+    String cvvnumber ="123";
+    String expdatenumber = "08/29/1992";
+
+
+    //Buy Icons for items
     @FindBy(xpath="//body/div[@id=" +
             "'root']/div[@class='sc-himrzO " +
             "gFAZYV']/div[1]/div[1]/div[1]/div[1]")
-        public WebElement headphonecart;
+        public WebElement headphonebuyicon;
 
-    String actualUrl = "http://localhost:3000/cart";
+//    @FindBy(xpath=" ")
+//    public WebElement  ;
+//
+//    @FindBy(xpath=" ")
+//    public WebElement  ;
 
+
+    //Nav Bar Tabs
     @FindBy(xpath="//*[name()='path' and contains(@d,'M15.55 13c')]")
-    public WebElement cartTab;
+    public WebElement cartnavbartabicon;
 
+    @FindBy(xpath="//strong[contains(text(),'ORDERS')]")
+    public WebElement ordersnavbartabicon;
+
+    @FindBy(xpath="//strong[contains(text(),'PROFILE')]")
+    public WebElement profilenavbartab;
+
+    @FindBy(xpath="//body/div[@id='root']/div[1]/div[1]/div[1]/h1[1]/img[1]")
+    public WebElement congomainpagetab;
+
+
+
+    //Display texts
     @FindBy(xpath="//span[@class='sc-hAZoDl gYrcjl']")
-    public WebElement headphonename;
+    public WebElement headphonenamedisplay;
 
     @FindBy(xpath="//b[normalize-space()='ID:']")
-    public WebElement headphoneID;
-
+    public WebElement headphoneidisplay;
 
     @FindBy(xpath="//div[@class='sc-dmRaPn jYjlfm']")
-    public WebElement headphonequan;
+    public WebElement headphonequantitydisplay;
 
+    @FindBy(xpath="//body/div[@id='root']/div[2]/div[1]/div[2]/div[2]/div[4]/span[2]")
+    public WebElement totalCostofOrderDisplay;
 
+    @FindBy(xpath="//body/div[@id='root']/div[2]/div[1]/div[2]/div[2]/div[1]/span[2]")
+    public WebElement subtotalcostoforderdisplay ;
+
+    @FindBy(xpath="//body/div[@id='root']/div[2]/div[1]/div[2]/div[2]/div[2]/span[2]")
+    public WebElement estimatedshippingcostdisplay ;
+
+    //Buttons
     @FindBy(xpath="//body/div[@id='root']/div[2]/div[1]/div[2]/div[2]/button[1]")
-    public WebElement checkoutbtn;
+    public WebElement checkoutnowbtn;
 
+    @FindBy(xpath="//body/div[@id='root']/main[2]/div[1]/form[1]/div[1]/div[4]/div[1]/button[1]")
+    public WebElement addpaymentsubmitbutton;
+
+    @FindBy(xpath="//tbody/tr[2]/td[4]/button[1]")
+    public WebElement deletepaymentbutton;
+
+    @FindBy(xpath="//body/div[@id='root']/main[1]/div[1]/form[1]/div[1]/div[1]/table[1]/tr[1]/label[1]/span[1]/input[1]")
+    public WebElement checkmarkpaymentbutton;
+
+    @FindBy(xpath="//body/div[@id='root']/main[1]/div[1]/form[1]/div[1]/button[1]")
+    public WebElement submitpaymentbutton;
+
+    @FindBy(xpath="//body/div[@id='root']/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/button[2]/*[1]")
+    public WebElement trashcanremoveitemquantityicon;
+
+    @FindBy(xpath="//body/div[@id='root']/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/button[3]")
+    public WebElement quantityincreasebtn;
+
+    @FindBy(xpath="//body/div[@id='root']/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div[2]/button[1]")
+    public WebElement quantitydecreasebtn  ;
+
+//    @FindBy(xpath=" ")
+//    public WebElement  ;
+//
+//    @FindBy(xpath=" ")
+//    public WebElement  ;
+
+    //Fields
     @FindBy(xpath="//input[@id='firstName']")
-    public WebElement firstnameinput;
+    public WebElement firstnameinputfield;
+
+    @FindBy(xpath="//input[@id='lastName']")
+    public WebElement lastnameinputfield;
 
     @FindBy(xpath="//input[@id='address1']")
-    public WebElement addressinput;
+    public WebElement addressinputfield;
 
     @FindBy(xpath="//input[@id='city']")
-    public WebElement cityInput;
+    public WebElement cityInputfield;
+
+    @FindBy(xpath="//input[@id='state']")
+    public WebElement stateInputfield;
+
+    @FindBy(xpath="//input[@id='country']")
+    public WebElement zipInputfield;
+
+    @FindBy(xpath="//input[@id='zip']")
+    public WebElement countryInputfield;
+
+    @FindBy(xpath="//input[@id='cardNumber']")
+    public WebElement cardnumberinputfield;
+
+    @FindBy(xpath="//input[@id='expDate']")
+    public WebElement expdateinputfield;
+
+    @FindBy(xpath="//input[@id='ccv']")
+    public WebElement cvvinputfield;
+
+//
+//    @FindBy(xpath=" ")
+//    public WebElement  ;
+//
+//    @FindBy(xpath=" ")
+//    public WebElement  ;
+//
+//    @FindBy(xpath="")
+//    public WebElement  ;
+
 }
