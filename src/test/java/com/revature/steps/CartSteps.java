bfcf05c4f3ab465869f0d7a5e9f0b705cd479b2b
Jeremy F McKay <jeremyfmckay@gmail.com>
2022-12-19 05:56:52 -0800

    Added some logic to new steps

--- src/test/java/com/revature/steps/CartSteps.java
+++ src/test/java/com/revature/steps/CartSteps.java
@@ -208,30 +208,41 @@ public class CartSteps {
 
     @And("The user types in a {string} in first name field.")
     public void theUserTypesInAInFirstNameField(String firstName) {
+        MainRunner.cartPage.firstnameinputfield.sendKeys(firstName);
     }
 
     @And("The user types in a {string} in last name field.")
     public void theUserTypesInAInLastNameField(String lastName) {
+        MainRunner.cartPage.lastnameinputfield.sendKeys(lastName);
+
+
+
+
     }
 
     @And("The user types in a {string} in address{int} field.")
     public void theUserTypesInAInAddressField(String address, int addNum) {
+        MainRunner.cartPage.addressinputfield.sendKeys(address);
     }
 
     @And("The user types in a {string} in city field.")
     public void theUserTypesInAInCityField(String city) {
+        MainRunner.cartPage.cityInputfield.sendKeys(city);
     }
 
     @And("The user types in a {string} in state field.")
     public void theUserTypesInAInStateField(String state) {
+        MainRunner.cartPage.stateInputfield.sendKeys(state);
     }
 
     @And("The user types in a {string} in zip field.")
     public void theUserTypesInAInZipField(String zip) {
+        MainRunner.cartPage.zipInputfield.sendKeys(zip);
     }
 
     @And("The user types in a {string} in country field.")
     public void theUserTypesInAInCountryField(String country) {
+        MainRunner.cartPage.countryInputfield.sendKeys(country);
     }
 
     @Then("They should be notified of a needed field.")
